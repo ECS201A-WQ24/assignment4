7dabe6c578dbaea1525ddaf0d80c22f8459f1540
Kaustav Goswami <kggoswami@ucdavis.edu>
2024-02-21 23:20:23 -0800

    201a: added reference code
    Signed-off-by: Kaustav Goswami <kggoswami@ucdavis.edu>

--- workloads/matmul/block_kj_multiply.h
+++ workloads/matmul/block_kj_multiply.h
@@ -4,7 +4,17 @@
 
 void multiply(double** A, double** B, double** C, int matrix_size, int block_size)
 {
-    // TODO: Put your matrix multiplication code with blocking over k, j here.
+    for (int kk = 0; kk < matrix_size; kk += block_size) {
+        for (int jj = 0; jj < matrix_size; jj += block_size) {
+            for (int i = 0; i < matrix_size; i++) {
+                for (int k = kk; k < kk + block_size; k++) {
+                    for (int j = jj; j < jj + block_size; j++) {
+                        C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+    }
 }
 
 #endif // __MATMUL_BLOCK_KJ_MULTIPLY_H__
